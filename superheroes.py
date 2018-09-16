86d95275e88d1824edf2130c972abc02e432550a
ErikBatista <erikbatista42@gmail.com>
2018-09-16 12:35:14 -0700

    actually starting project

---
+++ superheroes.py
@@ -0,0 +1,13 @@
+import random
+
+class Ability:
+
+    def __init__(self, name, attackStrength):
+        #initialize starting values
+        self.name = name
+        self.attackStrength = attackStrength
+    
+    def attack(self):
+        return 
+    
+    def updateAttack(self, attackStrength):
