3f1d95209c0badb64f7868efbb7c4a8424e29547
ErikBatista <erikbatista42@gmail.com>
2018-09-16 09:50:00 -0700

    experimenting

---
+++ dog.py
@@ -0,0 +1,10 @@
+dogs = list()
+dogs.append("German Shepherd")
+dogs.append("Poodle")
+print(dogs)
+
+class Dog: 
+    def bark(self):
+        print("Woof!")
+# we can't do this. That's like telling all dogs to bark.
+# Dog.bark() 
