c0aace202cde31a36104fb69b633e584d967eda0
ErikBatista <erikbatista42@gmail.com>
2018-09-17 09:34:35 -0700

    finished with 1 and 2 from tutorial

--- superheroes.py
+++ superheroes.py
@@ -2,12 +2,44 @@ import random
 
 class Ability:
 
-    def __init__(self, name, attackStrength):
-        #initialize starting values
-        self.name = name
-        self.attackStrength = attackStrength
+	def __init__(self, name, attack_strength):
+		self.name = name
+		self.attack_strength = attack_strength
+
+	def attack(self):
+		highestAttackVal = 800
+		lowestAttackVal = highestAttackVal // 2
+		randomAttackVal = random.randint(0, highestAttackVal)
+		return randomAttackVal
+
+	def update_attack(self, attack_strength):
+		self.attack_strength = attack_strength
+
+class Hero(Ability):
+
+	def __init__(self, name):
+		self.name = name
+		self.abilities = list()
+
+	def add_ability(self, ability):
+		self.abilities.append(ability)
+
+	def attack(self):
+		total = 0
+		for ab in self.abilities:
+			total += Ability.attack(ab)
+		return total
+
+
+
+if __name__ == "__main__":
+    hero = Hero("Wonder Woman")
+    print(hero.attack())
+    ability = Ability("Divine Speed", 300)
+    hero.add_ability(ability)
+    print(hero.attack())
+    new_ability = Ability("Super Human Strength", 800)
+    hero.add_ability(new_ability)
+    print(hero.attack())
+
     
-    def attack(self):
-        return 
-    
-    def updateAttack(self, attackStrength):
