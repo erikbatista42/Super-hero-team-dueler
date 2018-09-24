cc743224218bb90d48e228c978aaaa5be280feca
ErikBatista <erikbatista42@gmail.com>
2018-09-24 14:55:02 -0700

    completed more methods

--- superheroes.py
+++ superheroes.py
@@ -42,14 +42,13 @@ class Hero(Ability):
         self.kills = 0
 
     def defend(self):
+
         # This method should run the defend method on each piece of armor and calculate the total defense.
         # If the hero's health is 0, the hero is out of play and should return 0 defense points.
         print("armor list: ")
-        print(self.armors)
-        for heroArmors in self.armors:
-            print("defending armors:")
-            # print(heroArmors.defend())
-            # heroArmors.defend()
+        # print(self.armors)
+        if self.health == 0:
+            return 0
 
     def take_damage(self, damage_amt):
         # This method should subtract the damage amount from the hero's health.
@@ -60,7 +59,8 @@ class Hero(Ability):
 
     def add_kill(self, num_kills):
         # This method should add the number of kills to self.kills
-        self.kills += num_kills
+        # self.kills += num_kills
+        print(123)
 
     def add_ability(self, ability):
         self.abilities.append(ability)
@@ -87,6 +87,7 @@ class Team(Hero):
     def __init__(self, team_name):
         self.name = team_name
         self.heroes = list()
+        self.teamKills = 0
 
     def add_hero(self, Hero):
         self.heroes.append(Hero)
@@ -115,39 +116,58 @@ class Team(Hero):
     def attack(self, other_team):
         # This method should total our teams attack strength and call the defend() method on the rival team that is passed in.
         # It should call add_kill() on each hero with the number of kills made.
-        self.other_team = other_team
         totalAttackVal = 0
-
         for hero in self.heroes:
-            # print(hero.name)
+            print(hero.name)
             totalAttackVal += hero.attack()
+        kills = other_team.defend(totalAttackVal)
+        # self.teamKills += kills
+        for myHeroes in self.heroes:
+            myHeroes.add_kill(kills)
         print("Total team attack: {0} ".format(totalAttackVal))
-
-        other_team.defend(totalAttackVal)
 
     def defend(self, damage_amt):
         # This method should calculate our team's total defense.
         # Any damage in excess of our team's total defense should be evenly distributed amongst all heroes with the deal_damage() method.
         # Return number of heroes killed in attack.
-        self.damage_amt = damage_amt
+        defendVal = 0
+        for hero in self.heroes:
+            # defendVal += hero.defend()
+            print("")
 
     def deal_damage(self, damage):
         # Divide the total damage amongst all heroes.
         # Return the number of heros that died in attack.
-        self.damage = damage
+        numOfDeaths = 0
+        caluclateDamage = damage / len(self.heroes)
+        for hero in self.heroes:
+            numOfDeaths += hero.take_damage(caluclateDamage)
+        return numOfDeaths
 
     def revive_heroes(self, health=100):
         # This method should reset all heroes health to their original starting value.
         self.health = health
+        for hero in self.heroes:
+            hero.health = hero.start_health
 
     def stats(self):
-        print(123)
         # This method should print the ratio of kills/deaths for each member of the team to the screen.
         # This data must be output to the terminal.
+        print("SHOWING STATS")
+        print(self.name)
+        for hero in self.heroes:
+            print("HERO: {0}".format(self.name))
+        ability_names = []
+        for ability in self.abilities:
+            ability_names.append(ability.name)
+        print("Abilities: " + ", ".join(ability_names))
+        print("kills: {0} , deaths: {1}".format(self.kills, self.deaths))
 
     def update_kills(self):
-        print(123)
         # this method should update each hero when there is a team kill
+        print("updated kills:")
+        print(self.teamKills)
+        return self.teamKills
 
 
 if __name__ == "__main__":
